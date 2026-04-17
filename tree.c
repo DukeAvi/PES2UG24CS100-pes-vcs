132e05900ea15264545834bec310dee611829d3c
DukeAvi <dukeofxanadu@gmail.com>
2026-04-17 17:21:33 +0000

    feat(tree): finalize tree serialization and persist as object

--- tree.c
+++ tree.c
@@ -226,7 +226,16 @@ static int build_tree_level(const TreeIndex *index, const char *prefix, ObjectID
     }
 
     if (tree.count == 0) return -1;
-    return -1;
+    void *data = NULL;
+    size_t len = 0;
+    int serialize_result = tree_serialize(&tree, &data, &len);
+    if (serialize_result != 0) {
+        return -1;
+    }
+
+    int write_result = object_write(OBJ_TREE, data, len, id_out);
+    free(data);
+    return write_result;
 }
 
 // Build a tree hierarchy from the current index and write all tree
