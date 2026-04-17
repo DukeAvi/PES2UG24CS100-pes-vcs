175498c9bed46cc41a03c0b311c48690501d36b2
DukeAvi <dukeofxanadu@gmail.com>
2026-04-17 17:52:16 +0000

    feat(commit): add validation and tree construction logic to commit_create

--- commit.c
+++ commit.c
@@ -194,8 +194,14 @@ int head_update(const ObjectID *new_commit) {
 //
 // Returns 0 on success, -1 on error.
 int commit_create(const char *message, ObjectID *commit_id_out) {
-    // TODO: Implement commit creation
-    // (See Lab Appendix for logical steps)
-    (void)message; (void)commit_id_out;
-    return -1;
+    if (message == NULL || commit_id_out == NULL) {
+        return -1;
+    }
+
+    ObjectID tree_id = {0};
+    if (tree_from_index(&tree_id) != 0) {
+        return -1;
+    }
+
+    (void)tree_id;
 }
